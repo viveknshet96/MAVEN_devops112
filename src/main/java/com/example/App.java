0fbaa0ce96171ee1437caa4414d070581927998d
Vivek Shet <viveknshet123@gmail.com>
2026-01-22 00:10:26 +0530

    Update App class to print Docker Swarm message

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,8 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Docker Swarm CI/CD Pipeline Running");
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
